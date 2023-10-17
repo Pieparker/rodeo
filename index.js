93456b742dffcdd7468544a1cfc214bf3ea1856c
Scott <26013935+Pieparker@users.noreply.github.com>
2023-10-17 11:05:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
   onPreBuild: ({ utils }) => {
-    utils.build.cancelBuild('The build was cancelled by the plugin')
+    //utils.build.cancelBuild('The build was cancelled by the plugin')
   },
 }
